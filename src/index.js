4b7ca4b10499d3cf2446a89f3352b56b904ddbc2
Shun Iwashita <iwashunabc519@gmail.com>
2021-12-15 18:28:46 +0700

    update

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-function FirstHeader() {
+function Greeting() {
    return (
-      <h2>This is my first lesson of React</h2>
+      <div>
+         <h1>Hello world</h1>
+         <h2>This is my first lesson of React</h2>
+      </div>
+      // You can't put second element. Try to uncomment the code below and you will see the error.
+      // <div></div>
    );
 }
 
-ReactDom.render(<FirstHeader />, document.getElementById('root'));
+
+/////// same as the function above
+// const Greeting = () => {
+//    return React.createElement(
+//       'div', null, 
+//       React.createElement('h1', null, 'Hello world'),
+//       React.createElement('h2', null, 'This is my first lesson of React')
+//    );
+// }
+
+ReactDom.render(
+   <Greeting />
+   , document.getElementById('root')
+);
