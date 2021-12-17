24b150c5725a3970b4c44ab08a55540291f73a4d
Shun Iwashita <iwashunabc519@gmail.com>
2021-12-17 18:34:40 +0700

    update

--- src/index.js
+++ src/index.js
@@ -54,47 +54,47 @@ const Booklist = () => {
    return (
      <section className="booklist">
        <Book
-         src="https://m.media-amazon.com/images/I/81YHHiNPDNL._AC_UL640_QL65_.jpg"
+         img="https://m.media-amazon.com/images/I/81YHHiNPDNL._AC_UL640_QL65_.jpg"
          title="Tales of Life and Music"
          author="Dave Grohl"
        />
        <Book
-         src="https://images-na.ssl-images-amazon.com/images/I/911cmGSgcvL._AC_UL381_SR381,381_.jpg"
+         img="https://images-na.ssl-images-amazon.com/images/I/911cmGSgcvL._AC_UL381_SR381,381_.jpg"
          title="Will"
          author="Will Smith"
        />
        <Book
-         src="https://images-na.ssl-images-amazon.com/images/I/8144Vic9C5L._AC_UL127_SR127,127_.jpg"
+         img="https://images-na.ssl-images-amazon.com/images/I/8144Vic9C5L._AC_UL127_SR127,127_.jpg"
          title="I Love You to the Moon and Back"
          author="Amelia Hepworth"
        />
        <Book
-         src="https://images-na.ssl-images-amazon.com/images/I/51Ua3JTN8iS._SX338_BO1,204,203,200_.jpg"
+         img="https://images-na.ssl-images-amazon.com/images/I/51Ua3JTN8iS._SX338_BO1,204,203,200_.jpg"
          title="Big Shot Diary of a Wimpy Kid"
          author="Jeff Kinney"
        />
        <Book
-         src="https://m.media-amazon.com/images/I/91S+nNHdHSL._AC_UY436_FMwebp_QL65_.jpg"
+         img="https://m.media-amazon.com/images/I/91S+nNHdHSL._AC_UY436_FMwebp_QL65_.jpg"
          title="Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones"
          author="James Clear"
        />
        <Book
-         src="https://m.media-amazon.com/images/I/51DZeZw7K0L.jpg"
+         img="https://m.media-amazon.com/images/I/51DZeZw7K0L.jpg"
          title="Greenlight"
          author="Matthew McConaughey"
        />
        <Book
-         src="https://images-na.ssl-images-amazon.com/images/I/41lpkXqV+PL._SX418_BO1,204,203,200_.jpg"
+         img="https://images-na.ssl-images-amazon.com/images/I/41lpkXqV+PL._SX418_BO1,204,203,200_.jpg"
          title="History of the World Map by Map"
          author="DK"
        />
        <Book
-         src="https://images-na.ssl-images-amazon.com/images/I/51wSvLJG-rS._SX306_BO1,204,203,200_.jpg"
+         img="https://images-na.ssl-images-amazon.com/images/I/51wSvLJG-rS._SX306_BO1,204,203,200_.jpg"
          title="Astrophysics for People in a Hurry"
          author="Neil deGrasse Tyson"
        />
        <Book
-         src="https://images-na.ssl-images-amazon.com/images/I/61DTOTemyDL._SX330_BO1,204,203,200_.jpg"
+         img="https://images-na.ssl-images-amazon.com/images/I/61DTOTemyDL._SX330_BO1,204,203,200_.jpg"
          title="Underland: A Deep Time Journey"
          author="Robert Macfarlane"
        />
@@ -106,14 +106,14 @@ const Book = props => {
    // console.log(props);
    return (
       <article className="book">
-         <Image src={props.src}/>
+         <Image img={props.img}/>
          <Title title={props.title}/>
          <Author author={props.author}/>
       </article>
    );
 }
 
-const Image = props => <img src={props.src} href="book image" height="223px" className="bookimage"/>;
+const Image = props => <img src={props.img} href="book image" height="223px" className="bookimage"/>;
 
 const Title = props => <h4>{props.title}</h4>;
 
