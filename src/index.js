4065b252c18f2743ca05a11b563bdd4292b59f9a
Shun Iwashita <iwashunabc519@gmail.com>
2021-12-17 18:33:23 +0700

    update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import { logDOM } from "@testing-library/dom";
 import React from "react";
 import ReactDom from "react-dom";
 
@@ -17,7 +18,9 @@ function Greeting() {
     <React.Fragment>
       <h1 className="intro">Hello world</h1>
       <Person />
-      {/* you can set the style by addign two curly braces */}
+      {/* you can set the style by addign two curly braces 
+         the first bracket is to tell the computer that its inside is javaScript
+      */}
       <h2 style={ {
             padding: '10px 0',
             fontFamily: `'Courier New', Courier, monospace`
@@ -49,34 +52,71 @@ function Person() {
 
 const Booklist = () => {
    return (
-      <section className="booklist">
-         <Book />
-         <Book />
-         <Book />
-         <Book />
-         <Book />
-         <Book />
-         <Book />
-         <Book />
-         <Book />
-      </section>
+     <section className="booklist">
+       <Book
+         src="https://m.media-amazon.com/images/I/81YHHiNPDNL._AC_UL640_QL65_.jpg"
+         title="Tales of Life and Music"
+         author="Dave Grohl"
+       />
+       <Book
+         src="https://images-na.ssl-images-amazon.com/images/I/911cmGSgcvL._AC_UL381_SR381,381_.jpg"
+         title="Will"
+         author="Will Smith"
+       />
+       <Book
+         src="https://images-na.ssl-images-amazon.com/images/I/8144Vic9C5L._AC_UL127_SR127,127_.jpg"
+         title="I Love You to the Moon and Back"
+         author="Amelia Hepworth"
+       />
+       <Book
+         src="https://images-na.ssl-images-amazon.com/images/I/51Ua3JTN8iS._SX338_BO1,204,203,200_.jpg"
+         title="Big Shot Diary of a Wimpy Kid"
+         author="Jeff Kinney"
+       />
+       <Book
+         src="https://m.media-amazon.com/images/I/91S+nNHdHSL._AC_UY436_FMwebp_QL65_.jpg"
+         title="Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones"
+         author="James Clear"
+       />
+       <Book
+         src="https://m.media-amazon.com/images/I/51DZeZw7K0L.jpg"
+         title="Greenlight"
+         author="Matthew McConaughey"
+       />
+       <Book
+         src="https://images-na.ssl-images-amazon.com/images/I/41lpkXqV+PL._SX418_BO1,204,203,200_.jpg"
+         title="History of the World Map by Map"
+         author="DK"
+       />
+       <Book
+         src="https://images-na.ssl-images-amazon.com/images/I/51wSvLJG-rS._SX306_BO1,204,203,200_.jpg"
+         title="Astrophysics for People in a Hurry"
+         author="Neil deGrasse Tyson"
+       />
+       <Book
+         src="https://images-na.ssl-images-amazon.com/images/I/61DTOTemyDL._SX330_BO1,204,203,200_.jpg"
+         title="Underland: A Deep Time Journey"
+         author="Robert Macfarlane"
+       />
+     </section>
    );
 }
 
-const Book = () => {
+const Book = props => {
+   // console.log(props);
    return (
       <article className="book">
-         <Image />
-         <Title />
-         <Author />
+         <Image src={props.src}/>
+         <Title title={props.title}/>
+         <Author author={props.author}/>
       </article>
    );
 }
 
-const Image = () => <img src="https://m.media-amazon.com/images/I/81YHHiNPDNL._AC_UL640_QL65_.jpg" href="book image" width="180px" className="bookimage"/>;
+const Image = props => <img src={props.src} href="book image" height="223px" className="bookimage"/>;
 
-const Title = () => <h4>Tales of Life and Music</h4>;
+const Title = props => <h4>{props.title}</h4>;
 
-const Author = () => <p style={{ letterSpacing: "5px" }}>Dave Grohl</p>;
+const Author = props => <p style={{ letterSpacing: "5px" }}>{props.author}</p>;
 
 ReactDom.render(<Greeting />, document.getElementById("root"));
