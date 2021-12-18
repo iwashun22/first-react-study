204f8022a9e31e511fd47ef40bcd66b163169376
Shun Iwashita <iwashunabc519@gmail.com>
2021-12-18 09:09:02 +0700

    update

--- src/index.js
+++ src/index.js
@@ -50,10 +50,58 @@ function Person() {
    return <h3>by Shun Iwashita</h3>
 }
 
+const books = [
+   {
+      img: "https://m.media-amazon.com/images/I/81YHHiNPDNL._AC_UL640_QL65_.jpg",
+      title: "Tales of Life and Music",
+      author: "Dave Grohl"
+   },
+   {
+      img: "https://images-na.ssl-images-amazon.com/images/I/911cmGSgcvL._AC_UL381_SR381,381_.jpg",
+      title: "Will",
+      author: "Will Smith"
+   },
+   {
+      img: "https://images-na.ssl-images-amazon.com/images/I/8144Vic9C5L._AC_UL127_SR127,127_.jpg",
+      title: "I Love You to the Moon and Back",
+      author: "Amelia Hepworth"
+   },
+   {
+      img: "https://images-na.ssl-images-amazon.com/images/I/51Ua3JTN8iS._SX338_BO1,204,203,200_.jpg",
+      title: "Big Shot Diary of a Wimpy Kid",
+      author: "Jeff Kinney"
+   },
+   {
+      img: "https://m.media-amazon.com/images/I/91S+nNHdHSL._AC_UY436_FMwebp_QL65_.jpg",
+      title: "Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones",
+      author: "James Clear"
+   },
+   {
+      img: "https://m.media-amazon.com/images/I/51DZeZw7K0L.jpg",
+      title: "Greenlight",
+      author: "Matthew McConaughey"
+   },
+   {
+      img: "https://images-na.ssl-images-amazon.com/images/I/41lpkXqV+PL._SX418_BO1,204,203,200_.jpg",
+      title: "History of the World Map by Map",
+      author: "DK"
+   },
+   {
+      img: "https://images-na.ssl-images-amazon.com/images/I/51wSvLJG-rS._SX306_BO1,204,203,200_.jpg",
+      title: "Astrophysics for People in a Hurry",
+      author: "Neil deGrasse Tyson"
+   },
+   {
+      img: "https://images-na.ssl-images-amazon.com/images/I/61DTOTemyDL._SX330_BO1,204,203,200_.jpg",
+      title: "Underland: A Deep Time Journey",
+      author: "Robert Macfarlane"
+   }
+]
+
 const Booklist = () => {
    return (
      <section className="booklist">
-       <Book
+       {/* <Book
          img="https://m.media-amazon.com/images/I/81YHHiNPDNL._AC_UL640_QL65_.jpg"
          title="Tales of Life and Music"
          author="Dave Grohl"
@@ -97,7 +145,13 @@ const Booklist = () => {
          img="https://images-na.ssl-images-amazon.com/images/I/61DTOTemyDL._SX330_BO1,204,203,200_.jpg"
          title="Underland: A Deep Time Journey"
          author="Robert Macfarlane"
-       />
+       /> 
+       
+       This is very long code, so it will be better if I set the books in the array and iterate through it
+       */}
+
+       {/* This code is way shorter and  more efficient */}
+       {books.map(book => <Book book={book}/>)} 
      </section>
    );
 }
@@ -105,7 +159,7 @@ const Booklist = () => {
 // set the parameter for unique property
 const Book = props => {
    // console.log(props);
-   const { img, title, author } = props;
+   const { img, title, author } = props.book;
    return (
       <article className="book">
          <Image img={img}/>
